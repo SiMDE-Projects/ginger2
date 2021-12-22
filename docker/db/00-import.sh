4769a4882ad12fe34063b6730090d1f54de59a5a
César Richard <cesar.richard@weezevent.com>
2021-12-22 14:45:18 +0100

    Simplified DB docker init

--- docker/db/00-import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-STRUCT_TEMPFILE=/tmp/structure.sql
-DATA_TEMPFILE=/tmp/datas.sql
-
-echo "Loading $DUMP_TEMPFILE"
-mysql -h localhost -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $STRUCT_TEMPFILE
-mysql -h localhost -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $DATA_TEMPFILE
